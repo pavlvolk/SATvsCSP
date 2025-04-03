5fdf83f3f20c84682df4f6d62e6aa444326e031d
Paul Volk <paulvolk.pv@gmail.com>
2025-04-03 21:53:48 +0200

    Sat should hopefully be completely integrated

--- SudokuSAT.py
+++ SudokuSAT.py
@@ -7,7 +7,12 @@ def create_sudoku(size)->CNF:
     one_number_each(cnf, size)
     add_row_clauses(cnf, size)
     add_column_clauses(cnf, size)
-    add_grid_clauses(cnf, size)
+    if size == 4:
+        add_grid_clauses(cnf, 2, 2)
+    elif size == 6:
+        add_grid_clauses(cnf, 3, 2)
+    elif size == 9:
+        add_grid_clauses(cnf, 3, 3)
 
     return cnf
 
@@ -16,51 +21,51 @@ def one_number_each(cnf, size):
     #at least one number
     for i in range(1, size+1):
         for j in range(1, size+1):
-                cnf.append([varnum(i, j, k) for k in range(1, size+1)])
+                cnf.append([varnum(size, i, j, k) for k in range(1, size+1)])
 
     #at most one number
     for i in range(1, size+1):
         for j in range(1, size+1):
             for d1 in range(1, size):
                 for d2 in range(d1+1, size+1):
-                    cnf.append([-varnum(i, j, d1), -varnum(i, j, d2)])
+                    cnf.append([-varnum(size, i, j, d1), -varnum(size, i, j, d2)])
 
 
 def add_row_clauses(cnf, size):
     #at least every number in each row
     for r in range(1, size+1):
         for d in range(1, size+1):
-            cnf.append([varnum(r, c, d) for c in range(1, size+1)])
+            cnf.append([varnum(size, r, c, d) for c in range(1, size+1)])
 
     #at most every number per row
     for r in range(1, size+1):
         for d in range(1, size+1):
             for c1 in range(1, size):
                 for c2 in range(c1+1, size+1):
-                    cnf.append([-varnum(r, c1, d), -varnum(r, c2, d)])
+                    cnf.append([-varnum(size, r, c1, d), -varnum(size, r, c2, d)])
 
 
 def add_column_clauses(cnf, size):
     for c in range(1, size+1):
         for d in range(1, size+1):
-            cnf.append([varnum(r, c, d) for r in range(1, size+1)])
+            cnf.append([varnum(size, r, c, d) for r in range(1, size+1)])
 
 
     for c in range(1, size+1):
         for d in range(1, size+1):
             for r1 in range(1, size):
                 for r2 in range(r1+1, size+1):
-                    cnf.append([-varnum(r1, c, d), -varnum(r2, c, d)])
+                    cnf.append([-varnum(size, r1, c, d), -varnum(size, r2, c, d)])
 
 
-def add_grid_clauses(cnf, size):
-    for br in range(0, 3):
-        for bc in range(0, 3):
-            for d in range(1, 10):
+def add_grid_clauses(cnf, cblock_size, rblock_size):
+    for br in range(0, cblock_size):
+        for bc in range(0, rblock_size):
+            for d in range(1, rblock_size*cblock_size+1):
                 block_cells = [
-                    varnum(r, c, d)
-                    for r in range(1 + br * 3, 4 + br * 3)
-                    for c in range(1 + bc * 3, 4 + bc * 3)
+                    varnum(cblock_size*rblock_size, r, c, d)
+                    for r in range(1 + br * rblock_size, 1 + (br + 1) * rblock_size)
+                    for c in range(1 + bc * cblock_size, 1 + (bc + 1) * cblock_size)
                 ]
                 cnf.append(block_cells)  # At least one must be true
                 for i in range(len(block_cells)):
@@ -68,39 +73,50 @@ def add_grid_clauses(cnf, size):
                         cnf.append([-block_cells[i], -block_cells[j]])
 
 
-def varnum(r, c, d):
-    return 81 * (r - 1) + 9 * (c - 1) + d
+def varnum(size, r, c, d):
+    return size * size * (r - 1) + size * (c - 1) + d
 
 
-def add_sudoku_hints(cnf, puzzle):
-    for r in range(9):
-        for c in range(9):
-            if puzzle[r][c] != 0:  # If there is a clue
-                cnf.append([varnum(r + 1, c + 1, puzzle[r][c])])
-
-
-def solve_sudoku(size, hints):
+def solve_sudoku(size, hints)->(CNF, list):
     sudoku = create_sudoku(size)
-    add_sudoku_hints(sudoku, hints)
-    solver = Cadical195()
-    if solver.solve(sudoku):
+    add_sudoku_hints(sudoku, hints, size)
+    solver = Cadical195(sudoku)
+    if solver.solve():
         print("Sudoku is solvable")
     else:
         print("No solution found!")
+    return sudoku, solver.get_model()
 
 
-def add_sudoku_hints(cnf, puzzle):
-    for r in range(9):
-        for c in range(9):
+def add_sudoku_hints(cnf, puzzle, size):
+    for r in range(size):
+        for c in range(size):
             if puzzle[r][c] != 0:  # If there is a clue
-                cnf.append([varnum(r + 1, c + 1, puzzle[r][c])])
+                cnf.append([varnum(size, r + 1, c + 1, puzzle[r][c])])
 
 
-def check_uniqueness(cnf, solver):
-    model = solver.get_model()
-    cnf.append([-model[i] for i in range(0, 729)])
+def check_uniqueness(cnf, model, size):
+    negated_model = [-v for v in model if v > 0]
+    cnf.append(negated_model)
     with Cadical195(cnf) as usolver:
         if usolver.solve():
             print("The sudoku is not unique")
         else:
             print("The sudoku is unique")
+
+
+def compute_solution(model, size):
+    solution = [[0] * size for _ in range(size)]
+    for v in model:
+        if v > 0:
+            r = (v - 1) // (size * size) + 1
+            c = ((v - 1) % (size * size)) // size + 1
+            d = ((v - 1) % size) + 1
+            if 1 <= r <= size and 1 <= c <= size and 1 <= d <= size:
+                solution[r - 1][c - 1] = d
+    return solution
+
+
+def print_solution(solution):
+    for row in solution:
+        print(row)
--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-import CSP_Solver as CS
+from SudokuSAT import solve_sudoku, check_uniqueness, print_solution, compute_solution
 
-task = CS.CSP(variables=4)
-task.commonDomain(domain=[1, 2])
-task.addConstraint("value[1] != value[2]")
-task.addConstraint("value[1] != value[3]")
-task.addConstraint("value[2] != value[4]")
-task.addConstraint("value[3] != value[4]")
-task.solve_BackTrack(timeout=10)
+hints4 = [[0, 0, 0, 3],
+          [0, 4, 0, 0],
+          [1, 0, 0, 4],
+          [0, 0, 3, 0]]
 
-for i in range(1, 5):
-        print(f"Cell {i}: {task.value[i]}")
+hints6 = [[6, 2, 0, 5, 0, 3],
+          [0, 0, 0, 0, 0, 0],
+          [5, 0, 0, 0, 3, 0],
+          [0, 6, 0, 0, 2, 0],
+          [0, 0, 0, 3, 4, 6],
+          [3, 0, 6, 0, 0, 0],]
+
+hints9 = [[5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]]
+
+cnf4, model4 = solve_sudoku(4, hints4)
+check_uniqueness(cnf4, model4, 4)
+cnf6, model6 = solve_sudoku(6, hints6)
+check_uniqueness(cnf6, model6, 6)
+cnf9, model9 = solve_sudoku(9, hints9)
+check_uniqueness(cnf9, model9, 9)
+
+print_solution(compute_solution(model4, 4))
+print_solution(compute_solution(model6, 6))
+print_solution(compute_solution(model9, 9))
